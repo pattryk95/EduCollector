230127ec2547614d5461a002a65009f7dec09ddd
Patryk <patryk.bogdanski95@gmail.com>
2022-03-31 11:46:08 +0200

    Init  commit

---
+++ EduCollector/EduCollector.Domain/Entites/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCollector.Domain.Entites
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ EduCollector/EduCollector.Domain/Entites/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCollector.Domain.Entites
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime Data { get; set; }
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public string ImageUrl { get; set; }
+        public string Url { get; set; }
+        public int Rate { get; set; }
+    }
+}
