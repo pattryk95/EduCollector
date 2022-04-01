fda2f316bfbf571738390b250110d26eee052d7b
Patryk <patryk.bogdanski95@gmail.com>
2022-04-01 22:34:02 +0200

    add query handler

--- EduCollector/EduCollector.Application/Functions/Posts/GetPostsListQueryHandler.cs
+++ EduCollector/EduCollector.Application/Functions/Posts/GetPostsListQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using EduCollector.Domain;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,21 @@ namespace EduCollector.Application
 {
     public class GetPostsListQueryHandler : IRequestHandler<GetPostsListQuery, List<PostInListViewModel>>
     {
-        public Task<List<PostInListViewModel>> Handle(GetPostsListQuery request, CancellationToken cancellationToken)
+        private readonly IAsyncRepository<Post> _postRepository;
+        private readonly IMapper _mapper;
+
+        public GetPostsListQueryHandler(IAsyncRepository<Post> postRepository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _postRepository = postRepository;
+            _mapper = mapper;
+
+        }
+        public async Task<List<PostInListViewModel>> Handle(GetPostsListQuery request, CancellationToken cancellationToken)
+        {
+            var all = await _postRepository.GetAllAsync();
+            var allordered = all.OrderBy(x => x.Data);
+
+            return _mapper.Map<List<PostInListViewModel>>(all);
         }
     }
 }
